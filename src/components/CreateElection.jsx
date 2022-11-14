5913f8f7b1d98234853f892ee22d3bd078e96b0c
jbuyu <jb@myworkpay.com>
2022-11-14 20:51:24 +0300

    cleans up data

--- src/components/CreateElection.jsx
+++ src/components/CreateElection.jsx
@@ -1,72 +1,15 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import React, { useState } from 'react'
+import React from 'react'
 import { useForm  } from 'react-hook-form'
-import { createElection, deleteElection, getElections } from '../api'
-import ClipLoader from "react-spinners/ClipLoader";
-
-
-const override = {
-  display: "block",
-  margin: "0 auto",
-  borderColor: "red",
-};
-
 
 
 export default function CreateElection() {
-   let [loading, setLoading] = useState(true);
-   let [color, setColor] = useState("#ffffff");
-  const queryClient = useQueryClient()
-  const {register, handleSubmit} = useForm()
+    const { register, handleSubmit } = useForm();
 
 
-
-  const {
-    mutateAsync,
-    isLoading,
-    isError,
-  } = useMutation(createElection, {
-    onSuccess: ()=>{
-      console.log('mm')
-      queryClient.invalidateQueries("elections");
-    }
-  });
-
-    const onSubmit = async data => {
-      await mutateAsync(data);
-    };
-
-
-
-  const {isLoading:isElectionsLoading, isError:electionError, data} = useQuery({queryKey:["elections"], queryFn:getElections})
-
-
-  const {
-    mutateAsync: deleteMutateAsync,
-    isError: deleteIsError,
-    isLoading: deleteIsloading,
-  } = useMutation(deleteElection, {
-    onSuccess: () => {
-      queryClient.invalidateQueries("elections");
-    },
-  });
-
-  const deleteAnElection = async id => {
-    await deleteMutateAsync(id);
-  };
-
-  if (isElectionsLoading){
-    return (
-      <ClipLoader
-        color={color}
-        loading={loading}
-        cssOverride={override}
-        size={150}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
-    );
+  const onSubmit = (data)=>{
+    console.log('data', data)
   }
+  
     return (
       <div className="create-election">
         <h3>Create Election</h3>
@@ -78,20 +21,11 @@ export default function CreateElection() {
           <label htmlFor="party">Party</label>
           <input defaultValue="party" {...register("party")} />
           <button type="submit">
-            {
-              isLoading ? "loading": "Create Election"
-            }
+            Create Election
           </button>
         </form>
-        <div className="elections">
-          {data?.data?.data?.map(election => (
-            <div className="single-election" key={election._id}>
-              <p key={election._id}>{election.candidate}</p>
-              <button onClick={() => deleteAnElection(election._id)}>
-                Delete
-              </button>
-            </div>
-          ))}
+        <div className="">
+         
         </div>
       </div>
     );
--- src/components/Elections.jsx
+++ src/components/Elections.jsx
@@ -1,46 +1,12 @@
-import { useMutation, useQuery, useQueryClient  } from "@tanstack/react-query";
 import React from "react";
-import { deleteElection, getElections } from "../api";
 
 
 
 export default function Elections() {
-  const queryClient = useQueryClient()
-  const { data, isLoading, isError, is } = useQuery({
-    queryKey: ["Elections"],
-    queryFn: getElections,
-    // staleTime:5000
-  });
 
-
-  const {mutateAsync, isError:deleteIsError, isLoading:deleteIsloading} = useMutation(deleteElection, {
-    onSuccess:()=>{
-    queryClient.invalidateQueries("elections");
-    }
-  })
-
-
-
-  const deleteAnElection = async(id) =>{
-    await mutateAsync(id)
-  }
-
-
-  if (isError) {
-    return <p>Errored out</p>;
-  }
-
-  if (isLoading) {
-    return <p>Loading</p>;
-  }
   return (
     <div className="election-container">
-      {data?.data?.data?.map(election => (
-        <div className="posts" key={election._id}>
-          <p key={election._id}>{election.candidate}</p>
-          <button onClick={() => deleteAnElection(election._id)}>Delete</button>
-        </div>
-      ))}
+     <h3>Elections</h3>
     </div>
   );
 }
